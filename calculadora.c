45ebac68023d46581689e7b6bb66c499c14851e7
mcpt000 <62105954+mcpt000@users.noreply.github.com>
2020-03-29 19:16:57 -0600

    Add files via upload

---
+++ calculadora.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<math.h>
+#include <stdlib.h>
+#include <string.h>
+
+float add(float a, float b)
+{
+    return a+b;
+}
+
+float subtr(float a, float b)
+{
+    return a-b;
+}
+
+float divi(float a, float b)
+{
+    return a/b;
+}
+
+float multip(float a, float b)
+{
+    return a*b;
+}
+
+int main (int argc, char** argv)
+{
+    char carac = argv[1][0];
+	float a=atof(argv[2]);
+	float b=atof(argv[3]);
+    printf("%c\n %f\n %f\n", carac, a, b);
+    switch (carac)
+	{
+        case '+':
+            printf("The result of the addition is: %.3f\n", add(a,b));
+            break;
+
+        case '-':
+            printf("The result of the subtraction is: %.3f\n", subtr(a,b));
+            break;
+
+        case '/':
+            printf("The result of the division is: %.3f\n", divi(a,b));
+            break;
+
+        case '*':
+            printf("The result of the multiplication is: %.3f\n", multip(a,b));
+            break;
+
+        default:
+            printf("Syntax error");
+            break;
+	}
+}
